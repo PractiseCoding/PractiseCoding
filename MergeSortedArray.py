e00edad7c59a4a94ec2e5bfa300d23f349f5d7bf
PractiseCoding <87877220+PractiseCoding@users.noreply.github.com>
2021-07-31 19:47:02 -0400

    Create MergeSortedArray.py

---
+++ MergeSortedArray.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        
+        fullLength=m+n-1
+        while m>0 and n>0 :
+            if nums1[m-1] > nums2[n-1]:
+                nums1[fullLength]=nums1[m-1]
+                m=m-1
+            else:
+                nums1[fullLength]=nums2[n-1]
+                n=n-1
+            
+            fullLength=fullLength-1
+            
+        while n>0:
+            nums1[fullLength]=nums2[n-1]
+            n=n-1
+            fullLength=fullLength-1
